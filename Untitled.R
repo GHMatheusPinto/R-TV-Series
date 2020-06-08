d92c8a0ee9b089869ab3d2789ed832748e380932
GHMatheusPinto <matheuspinto@outlook.com>
2020-06-08 11:54:16 -0300

    adicionando analises por servico

--- Untitled.R
+++ Untitled.R
@@ -35,3 +35,31 @@ filter(tv, IMDb>="8.0")
 ggplot(tv, aes(x=as.factor(tv$Age), y=tv$IMDb)) + 
   geom_boxplot(fill="slateblue", alpha=0.2) + 
   xlab("Faixa etária") + ylab("Nota no IMDb")
+
+serv_netflix = tv%>%filter(Netflix=="1")%>%select(2,3,4,5)
+serv_netflix
+count(serv_netflix)
+
+serv_prime = tv%>%filter(Prime.Video=="1")%>%select(2,3,4,5)
+serv_prime
+count(serv_prime)
+
+serv_hulu = tv%>%filter(Hulu=="1")%>%select(2,3,4,5)
+serv_hulu
+count(serv_hulu)
+
+serv_disney = tv%>%filter(Disney.=="1")%>%select(2,3,4,5)
+serv_disney
+count(serv_disney)
+
+
+
+
+
+
+
+
+
+
+
+
